2b40e7e256003e0023ae512082f53c338759d7fc
Heisenberg200 <65282454+Heisenberg200@users.noreply.github.com>
2022-04-20 22:54:27 +0300

    lab4

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+// COMPONENTS
+import Navbar from "../src/components/Navbar";
+import Home from "../src/components/Home";
+
+const Index = () => {
+  return (
+    <Box>
+      <Navbar />
+      <Home />
+    </Box>
+  );
+};
+
+export default Index;
+
+
+// Thats it for today :)
+// Hope you got the source code from github
